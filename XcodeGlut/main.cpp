f36d1bbc90221e8b7e678cc91d7d8d052189b28b
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-10-20 02:43:02 +0530

    Initial Commit

---
+++ XcodeGlut/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  XcodeGlut
+//
+//  Created by Robin Malhotra on 20/10/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
