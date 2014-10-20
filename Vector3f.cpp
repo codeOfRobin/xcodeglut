a4f68ddf0e138bdc56300ebf3fc86e232019d940
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-10-20 23:42:18 +0530

    Tutorial 4 complete

---
+++ Vector3f.cpp
@@ -0,0 +1,9 @@
+//
+//  Vector3f.cpp
+//  XcodeGlut
+//
+//  Created by Robin Malhotra on 20/10/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#include "Vector3f.h"
--- XcodeGlut/main.cpp
+++ XcodeGlut/main.cpp
@@ -7,12 +7,67 @@
 //
 
 #include <iostream>
+#include <OpenGL/OpenGL.h>
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#include <GLUT/GLUT.h>
+#import "Vector3f.h"
 
-int main(int argc, const char * argv[])
+vector3f CAMERA_ROTATION;
+vector3f CAMERA_POSITION;
+using namespace std;
+const int WINDOW_HEIGHT=720;
+const int WINDOW_WIDTH=1200;
+const char* WINDOW_TITLE="something";
+
+void reshape(int w, int h)
 {
+    float aspectRatio;
+    if (h==0)
+    {
+        h=1;
+    }
+    aspectRatio=float(w)/float(h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w ,h);
+    gluPerspective(45, aspectRatio, 0.01, 100000000);
+    glMatrixMode(GL_MODELVIEW);
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    
+    glTranslatef(-CAMERA_POSITION.x, -CAMERA_POSITION.y, -CAMERA_POSITION.z);
+    glRotatef(CAMERA_ROTATION.x, 1, 0, 0);
+    glRotatef(CAMERA_ROTATION.y, 0, 1, 0);
+    glRotatef(CAMERA_ROTATION.z, 0, 0, 1);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1, 0, 0);
+    glVertex3f(-1, -1.0f,-3);
+    glColor3f(0, 1, 0);
+    glVertex3f(0.0f, 1.0f,-3);
+    glColor3f(0, 0, 1);
+    glVertex3f(1.0f, -1.0f,-3);
+    glEnd();
+    
+    glutSwapBuffers();
+}
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc,char ** argv)
+{
+    CAMERA_ROTATION.z=30.0f;
+    
+    glutInit(&argc, argv);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
+    
+    glutCreateWindow(WINDOW_TITLE);
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
     return 0;
 }
 
